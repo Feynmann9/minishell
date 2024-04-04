175c4cf34aacf428e814346d499041dcb4f1db98
feynmann9 <p4pyy.contact@gmail.com>
2024-04-04 18:26:18 +0200

    commit libft

---
+++ includes/minishell.h
@@ -0,0 +1,14 @@
+#ifndef MINISHELL
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <stdio.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+
+
+
+#endif
