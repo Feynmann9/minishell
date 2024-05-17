33df08a40d97d4bad8f9c8608513b6293807aa98
gmarquis <gregory.marquiset@gmail.com>
2024-05-17 03:25:55 +0200

    fin des tests et definition des etapes

--- parse/srcs/input_reading.c
+++ parse/srcs/input_reading.c
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse.h                                            :+:      :+:    :+:   */
+/*   input_reading.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmarquis <gmarquis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/14 16:31:51 by gmarquis          #+#    #+#             */
-/*   Updated: 2024/05/17 03:23:01 by gmarquis         ###   ########.fr       */
+/*   Created: 2024/05/17 02:56:29 by gmarquis          #+#    #+#             */
+/*   Updated: 2024/05/17 03:22:47 by gmarquis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSE_H
-# define PARSE_H
-
-#include "../includes/minishell.h"
-
-typedef struct s_prompt
-{
-	char			**cmd; //[0]cat [1]-e [2]etc...
-	char			**infiles;
-	char			**outfiles;
-	struct s_cmd	*next;
-}					t_prompt;
-
-//		input_reading.c		//
-
-#endif
+#include "../parse.h"
--- parse/srcs/main.c
+++ parse/srcs/main.c
@@ -6,23 +6,26 @@
 /*   By: gmarquis <gmarquis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:50:04 by gmarquis          #+#    #+#             */
-/*   Updated: 2024/05/16 04:56:24 by gmarquis         ###   ########.fr       */
+/*   Updated: 2024/05/17 03:04:38 by gmarquis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parse.h"
 
-int	main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **envp)
 {
-	int			i;
-	t_prompt	prompt;
+	int	i;
 
-	if (argc != 1 && argv[1][0] == '\0')
-		ft_exit(2, "Erreur: only one argument accepted.\n");
-	ft_bzero(&prompt, sizeof(prompt));
-	i = 0;
-	prompt.env = env;
-	ft_parse_prompt(&prompt);
-	ft_print_cmd(prompt.cmd);
+	if (argc != 2)
+		ft_exit(2, "Error: bad argument format.\n");
+	if (argv)
+	{
+		i = 0;
+		while (envp[i])
+		{
+			ft_printf("%s\n", envp[i]);
+			i++;
+		}
+	}
 	return (0);
 }
