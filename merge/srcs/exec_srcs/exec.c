8562bfcdbea2fa21bd9a58db3728e05e31683666
feynmann9 <p4pyy.contact@gmail.com>
2024-07-24 20:55:08 +0200

    commit for exec

--- merge/srcs/exec_srcs/exec.c
+++ merge/srcs/exec_srcs/exec.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmarquis <gmarquis@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jpp <jpp@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 22:29:05 by gmarquis          #+#    #+#             */
-/*   Updated: 2024/07/24 00:46:57 by gmarquis         ###   ########.fr       */
+/*   Updated: 2024/07/24 20:54:25 by jpp              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-
+/*
 char	**ft_str(t_infos *infos, char **str, char **args)
 {
 	int	i;
@@ -29,7 +29,7 @@ char	**ft_str(t_infos *infos, char **str, char **args)
 	while (args[j] != NULL)
 	{
 		// || ft_strcmp(args[j], ">") || !ft_strcmp(args[j], "<") || !ft_strcmp(args[j], ">>") || !ft_strcmp(args[j], "<<")
-		if (ft_strcmp(args[j], "|") == 0)
+		if (ft_strcmp(args[j], "|") == 0 || ft_strcmp(args[j], ">") == 0 || ft_strcmp(args[j], "<") == 0 || ft_strcmp(args[j], ">>") == 0 || ft_strcmp(args[j], "<<") == 0)
 			break ;
 		str[i] = ft_strdup(args[j]);
 		if (str[i] == NULL)
@@ -77,17 +77,6 @@ void	ft_multi(t_infos *infos)	//t_base *base, char **env
 		exit(EXIT_FAILURE);
 	}
 
-/*
-infos->tok->cmd --> [0] ls
-infos->tok->cmd --> [1] -l
-
-if(infos->tok->NEXT)
-	infos->tok = infos->tok->NEXT
-
-infos->tok->cmd --> [0] grep
-infos->tok->cmd --> [1] minishell
-*/
-
 	pid2 = fork();
 	if (pid2 < 0)
 		return ;
@@ -114,6 +103,7 @@ infos->tok->cmd --> [1] minishell
 	waitpid(pid1, NULL, 0);
 	waitpid(pid2, NULL, 0);
 }
+*/
 /*
 void	handle_redirections(t_infos *infos)
 {
@@ -296,66 +286,101 @@ void handle_redirections(t_infos *infos)
     int pid;
     char *path_env;
     char *full_path = NULL;
-    char **args;
     t_tok *current_tok;
 
     if (infos->envp == NULL || infos == NULL)
         return;
-    // Allocation de mémoire pour les pipes
+
+    // allocation pour le nombre de pipes quil y a
+    // le pipe connecte les deux parties seulement si besoin (cat < Makefile > out | echo mathieu)
+    // mais echo n a pas d entree de cat, car ca le fou dans out
+    // Pipe (|) : Relie deux programmes pour que la sortie du premier devienne l'entrée du second
+    // Redirection (>, <) : Change où les données vont (vers ou depuis un fichier)
     pipes = malloc(sizeof(int *) * infos->count_pipes);
-    for (i = 0; i < infos->count_pipes; i++) {
+    i = 0;
+    while (i < infos->count_pipes)
+    {
         pipes[i] = malloc(sizeof(int) * 2);
         if (pipe(pipes[i]) == -1)
-            return;
+            return ;
+        i++;
     }
+    //pipes[i][0] : Descripteur de lecture.
+    //pipes[i][1] : Descripteur d'écriture.
     current_tok = infos->tok;
-    for (i = 0; i <= infos->count_pipes; i++) {
+    i = 0;
+    while (i <= infos->count_pipes)
+    {
+        // creation processus pour chaque commande dans la chaine de pipes
         pid = fork();
         if (pid < 0)
             return;
-        if (pid == 0) {
+        if (pid == 0)
+        {
             // Gestion des redirections d'entrée
-            if (current_tok->infile) {
+            if (current_tok->infile)
+            {
+                //Configurer le programme pour lire depuis un fichier au lieu du clavier.
                 t_files *infile = current_tok->infile;
-                while (infile->NEXT)
+                while (infile->NEXT) // Trouver le dernier fichier dans la liste
                     infile = infile->NEXT;
+                //Utiliser la fonction open() pour ouvrir le fichier en mode lecture
                 int fd_in = open(infile->file, O_RDONLY);
                 if (fd_in < 0)
                     exit(EXIT_FAILURE);
+                //Utiliser dup2() pour rediriger l’entrée standard (stdin) vers le fichier ouvert.
                 if (dup2(fd_in, STDIN_FILENO) < 0)
                     exit(EXIT_FAILURE);
                 close(fd_in);
             }
+
             // Gestion des redirections de sortie
-            if (current_tok->outfile) {
+            if (current_tok->outfile)
+            {
                 t_files *outfile = current_tok->outfile;
                 while (outfile->NEXT)
                     outfile = outfile->NEXT;
                 int fd_out;
                 if (outfile->type == TOKEN_REDIRECT_OUT)
-                    fd_out = open(outfile->file, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+                    fd_out = open(outfile->file, O_CREAT | O_WRONLY | O_TRUNC, 0666); // Créer ou écraser le fichier
                 else if (outfile->type == TOKEN_REDIRECT_APPEND)
-                    fd_out = open(outfile->file, O_CREAT | O_WRONLY | O_APPEND, 0666);
+                    fd_out = open(outfile->file, O_CREAT | O_WRONLY | O_APPEND, 0666); // Ajouter au fichier existant
                 if (fd_out < 0)
                     exit(EXIT_FAILURE);
-                if (dup2(fd_out, STDOUT_FILENO) < 0)
+                if (dup2(fd_out, STDOUT_FILENO) < 0) // Rediriger stdout vers le fichier
                     exit(EXIT_FAILURE);
                 close(fd_out);
             }
             // Gestion des pipes
-            if (i > 0) {
+            if (i > 0)
+            {
+                //Si nous ne sommes pas à la première commande
+                close(pipes[i - 1][1]); // Fermer l'extrémité d'écriture du pipe précédent
+                //Nous redirigeons l'entrée standard (stdin) vers l'extrémité de lecture du tuyau précédent.
                 if (dup2(pipes[i - 1][0], STDIN_FILENO) < 0)
                     exit(EXIT_FAILURE);
+                close(pipes[i - 1][0]);
             }
-            if (i < infos->count_pipes) {
+            //la sortie du premier devient l entree du prochain
+            if (i < infos->count_pipes && current_tok->outfile == 0)
+            {
+                close(pipes[i][0]); // Fermer l'extrémité de lecture du pipe actuel
+                //Cela signifie que tout ce que la commande(1er) écrit sera envoyé au tuyau.
+                //en gros on dit que la sortie de la commande sera envoye au pipe
                 if (dup2(pipes[i][1], STDOUT_FILENO) < 0)
                     exit(EXIT_FAILURE);
+                close(pipes[i][1]);
             }
-            // Fermeture des descripteurs de fichiers inutilisés
-            for (int j = 0; j < infos->count_pipes; j++) {
+            // Fermeture des descripteurs de fichiers inutilisés dans les processus enfants
+            // cette une sorte de securite
+            int j = 0;
+            while (j < infos->count_pipes)
+            {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
+                j++;
             }
+
             // Exécution de la commande
             path_env = get_env_value(infos->tmp_env, "PATH");
             if (!path_env)
@@ -363,27 +388,37 @@ void handle_redirections(t_infos *infos)
             full_path = find_command(infos, current_tok->cmd[0], path_env);
             if (!full_path)
                 exit(EXIT_FAILURE);
-            args = current_tok->cmd; // Assurez-vous que cmd est correctement formaté
-            execve(full_path, args, infos->envp);
+            execve(full_path, current_tok->cmd, infos->envp);
             exit(EXIT_FAILURE);
         }
         current_tok = current_tok->NEXT;
+        i++;
     }
-    // Fermeture des descripteurs de fichiers dans le processus parent
-    for (i = 0; i < infos->count_pipes; i++) {
+
+    // Fermeture des descripteurs de fichiers inutilisés dans le processus parent
+    i = 0;
+    while (i < infos->count_pipes)
+    {
         close(pipes[i][0]);
         close(pipes[i][1]);
+        i++;
     }
-    // Attente de tous les processus enfants
-    for (i = 0; i <= infos->count_pipes; i++)
+
+    // Attente de tous les processus enfants se terminent
+    i = 0;
+    while (i <= infos->count_pipes)
+    {
         wait(NULL);
+        i++;
+    }
     // Libération de la mémoire allouée pour les pipes
-    for (i = 0; i < infos->count_pipes; i++)
-        free(pipes[i]);
+    i = 0;
+    while (i < infos->count_pipes)
+        free(pipes[i++]);
     free(pipes);
 }
 
-
+/*
 void handle_pipe(t_infos *infos) {
     int *pipes; // Tableau de descripteurs de fichiers pour les pipes
     int num_pipes = infos->count_pipes;
@@ -466,6 +501,7 @@ void handle_pipe(t_infos *infos) {
     // Libérer la mémoire
     free(pipes);
 }
+*/
 /*
 void	handle_redirections(t_tok *tok)
 {
